ac4b1c3422a1c54e3cf44113b725d960faabe9db
José Pedro Sousa <github@zepedro.me>
2022-12-12 12:00:49 +0000

    chore: testing the deployment on a new repo

--- terraform/server.sh
+++ terraform/server.sh
@@ -9,10 +9,10 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 
 ## Cloning Nightfall repo and installing the NPM version used
-git clone -b ${git_branch} https://github.com/maticnetwork/nightfall_phase2ceremony.git
+git clone -b ${git_branch} https://github.com/NightfallRollup/phase2ceremony.git
 echo "Commit hash ${commit_hash}"
 
-cd nightfall_phase2ceremony/serve
+cd phase2ceremony/serve
 nvm install
 nvm use
 
