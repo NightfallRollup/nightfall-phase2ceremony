02351e0bee86274c51d8b15699a000ffc27f4f59
José Pedro Sousa <github@zepedro.me>
2022-12-15 15:25:55 +0000

    fix: removing constraining validations in joi, passing the circuit name as s3 folder name

--- serve/schemas/upload.js
+++ serve/schemas/upload.js
@@ -2,15 +2,9 @@ import joi from 'joi';
 
 export const uploadContribSchema = joi.object({
   name: joi.string().alphanum().max(40).required(),
-  circuit: joi
-    .string()
-    .valid('deposit', 'withdraw', 'transfer', 'transform', 'tokenise', 'burn')
-    .required(),
+  circuit: joi.string().required(),
 });
 
 export const uploadBeaconSchema = joi.object({
-  circuit: joi
-    .string()
-    .valid('deposit', 'withdraw', 'transfer', 'transform', 'tokenise', 'burn')
-    .required(),
+  circuit: joi.string().required(),
 });
